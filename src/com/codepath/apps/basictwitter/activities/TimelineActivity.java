051ec83ddb859b7e351cdd269ae13d142508a9a5
i0jupiter <bhashinee.1911+git@gmail.com>
2014-06-28 20:42:19 -0700

    ActionBar tabs work partially -- the Compose icon is gone! Also, how to refresh a specific tab when we return from ComposeActivity?

--- src/com/codepath/apps/basictwitter/activities/TimelineActivity.java
+++ src/com/codepath/apps/basictwitter/activities/TimelineActivity.java
@@ -1,5 +1,7 @@
 package com.codepath.apps.basictwitter.activities;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -8,7 +10,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.codepath.apps.basictwitter.R;
+import com.codepath.apps.basictwitter.fragments.HomeTimelineFragment;
+import com.codepath.apps.basictwitter.fragments.MentionsTimelineFragment;
 import com.codepath.apps.basictwitter.fragments.TweetListFragment;
+import com.codepath.apps.basictwitter.listeners.FragmentTabListener;
 
 /**
  * Main activity showing the Twitter timeline of the user.
@@ -20,6 +25,9 @@ public class TimelineActivity extends FragmentActivity {
 
 	private final int COMPOSE_TWEET_REQUEST_CODE = 100;
 	
+	private ActionBar actionBar;
+	private Tab homeTab;
+	private Tab mentionsTab;
 	private TweetListFragment tweetListFragment;
 	
 	@Override
@@ -27,9 +35,11 @@ public class TimelineActivity extends FragmentActivity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
+		setupTabs();
 		
-		tweetListFragment = (TweetListFragment) getSupportFragmentManager()
-					.findFragmentById(R.id.fragment_timeline);
+//		//actionBar.getTabAt(0).getCustomView().refr
+//		tweetListFragment = (TweetListFragment) getSupportFragmentManager()
+//					.findFragmentById(R.id.);
 	}
 
 	// Create the action bar for this activity
@@ -58,7 +68,35 @@ public class TimelineActivity extends FragmentActivity {
 		if (resultCode == RESULT_OK && requestCode == COMPOSE_TWEET_REQUEST_CODE) {
 			
 			Log.d("debug", "Trying to refresh timeline after composing tweet.");
-			tweetListFragment.refreshTimeline();
+			//tweetListFragment.refreshTimeline();
 		}
+	}
+	
+	
+	/* Private methods */
+	
+	private void setupTabs() {
+		
+		actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setDisplayShowTitleEnabled(true);
+
+		homeTab = actionBar
+		    .newTab()
+		    .setText("Home")
+		    .setTag("HomeTimelineFragment")
+		    .setTabListener(new FragmentTabListener<HomeTimelineFragment>(R.id.flContainer, this,
+                        "Home", HomeTimelineFragment.class));
+
+		actionBar.addTab(homeTab);
+		actionBar.selectTab(homeTab);
+
+		mentionsTab = actionBar
+		    .newTab()
+		    .setText("Mentions")
+		    .setTag("MentionsTimelineFragment")
+		    .setTabListener(new FragmentTabListener<MentionsTimelineFragment>(R.id.flContainer, this,
+                        "Mentions", MentionsTimelineFragment.class));
+		actionBar.addTab(mentionsTab);
 	}
 }
